2f685ec34072e10b8ce24b5e54cc86f167c6cd7c
amelielefrancv <amelielefrancv@gmail.com>
2025-06-27 01:19:17 -0400

    Add files via upload

---
+++ Entrega_05/docs/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Webstory cargada correctamente 🎧');
+
+    // Ejemplo de interacción futura: botones, gráficos, etc.
+    const botones = document.querySelectorAll('button');
+    botones.forEach(boton => {
+        boton.addEventListener('click', () => {
+            alert('Esta función estará disponible pronto 🎶');
+        });
+    });
+});
